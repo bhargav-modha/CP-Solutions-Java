77e68b6b67df2da9708a7c27662e842013330bdf
Bhargav Modha <56217073+bhargav-modha@users.noreply.github.com>
2022-06-22 14:23:24 +0530

    Committing files

---
+++ CodeForces/Java 11/978B | File Name/141422207.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+ 
+public class B_File_Name {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+ 
+        int n = sc.nextInt();
+        String str = sc.next();
+        int count = 0;
+ 
+        while(str.contains("xxx"))
+        {
+            String temp = str.substring(0, str.indexOf("xxx"))+str.substring(str.indexOf("xxx")+1);
+            str = temp;
+            count++;
+        }
+ 
+        out.println(count);
+        out.close();
+    }
+ 
+    public static PrintWriter out;
+ 
+    public static class MyScanner {
+ 
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        int[] readArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+        long[] readLongArray(int n) {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
