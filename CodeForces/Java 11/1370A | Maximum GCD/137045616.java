8abdf58af14e59fd8fef7d828fad5ff01319fb0b
Bhargav Modha <56217073+bhargav-modha@users.noreply.github.com>
2022-06-22 14:28:34 +0530

    Committing files

---
+++ CodeForces/Java 11/1370A | Maximum GCD/137045616.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class A_Maximum_GCD
+{
+    public static void main(String Args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {        
+            System.out.println((sc.nextInt())/2);
+        }
+    }
+}
