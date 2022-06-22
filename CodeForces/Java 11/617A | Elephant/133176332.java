55903c6cf0534122b32117ad2c4890080ef510cf
Bhargav Modha <56217073+bhargav-modha@users.noreply.github.com>
2022-06-22 14:33:41 +0530

    Committing files

---
+++ CodeForces/Java 11/617A | Elephant/133176332.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ 
+public class A_Elephant
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((sc.nextInt() + 4) / 5);
+    }
+}
