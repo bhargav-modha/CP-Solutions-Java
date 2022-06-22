68f70f234738dc269979f1d69b8230389c907950
Bhargav Modha <56217073+bhargav-modha@users.noreply.github.com>
2022-06-22 14:32:40 +0530

    Committing files

---
+++ CodeForces/Java 11/200B | Drinks/134029084.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class B_Drinks
+{
+    public static void main(String Args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        double sum=0;
+ 
+        int n = sc.nextInt();
+ 
+        for(int i=0;i<n;i++)
+        {
+            sum += (float)sc.nextInt();
+        }
+        System.out.printf("%.12f",sum/n);
+    }
+}
