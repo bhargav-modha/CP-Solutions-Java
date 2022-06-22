fc6cfa2a96d672896681d9bead489ada8e947d57
Bhargav Modha <56217073+bhargav-modha@users.noreply.github.com>
2022-06-22 14:34:50 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1594A | Consecutive Sum Riddle/131238933.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+         long long int n;
+         cin>>n;
+         cout<<-(n-1)<<" "<<n<<endl;
+    }
+    return 0;
+}   
